93a46ec1ef9b32eb97341a305089d44086582263
Munchie3319 <babyxface@gmail.com>
2012-02-03 15:06:08 -0500

    update #3

--- class1/labs/lpthw_ex3.py
+++ class1/labs/lpthw_ex3.py
@@ -1,21 +1,33 @@
+# print sentence
 print "I will now count my chickens:"
 
+# print the total amount of Hens and Rooster after math formula calculation
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
+# print sentence
 print "Now I will count the eggs:"
 
+#print final amount after math formula calculation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# print question sentence
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# print false to question sentence
 print 3 + 2 < 5 - 7
 
+#print sentence and answer to math formula calculation
 print "What is 3 + 2?", 3 + 2
-print "what is 5 - 7?", 5 - 7
+print "What is 5 - 7?", 5 - 7
 
-print "oh, that's why it's False."
+# print sentence
+print "Oh, that's why it's False."
 
+#print sentence
 print "How about some more."
 
-print "Is it greater?", 5> -2
+# print question sentences and True or False answers
+print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
