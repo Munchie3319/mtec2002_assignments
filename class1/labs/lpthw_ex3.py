4165ade0b280128df3d4ea84c76e81ddb3240a15
Munchie3319 <babyxface@gmail.com>
2012-02-03 15:52:21 -0500

    #3 update

--- class1/labs/lpthw_ex3.py
+++ class1/labs/lpthw_ex3.py
@@ -2,24 +2,24 @@
 print "I will now count my chickens:"
 
 # print the total amount of Hens and Rooster after math formula calculation
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25.0 + 30 / 6
+print "Roosters", 100.0 - 25 * 3 % 4
 
 # print sentence
 print "Now I will count the eggs:"
 
 #print final amount after math formula calculation
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4.0 % 2 - 1 / 4.0 + 6
 
 # print question sentence
-print "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3.0 + 2 < 5.0 - 7?"
 
 # print false to question sentence
-print 3 + 2 < 5 - 7
+print 3.0 + 2 < 5.0 - 7
 
 #print sentence and answer to math formula calculation
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3.0 + 2?", 3.0 + 2
+print "What is 5.0 - 7?", 5.0 - 7
 
 # print sentence
 print "Oh, that's why it's False."
@@ -28,6 +28,9 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # print question sentences and True or False answers
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
+
+# I dont understand the extra credit question 2, python is already in my mac
+
