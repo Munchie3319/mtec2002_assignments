bb43934f655d6d4210936409f20729768a8c7251
Munchie3319 <munchie3319@gmail.com>
2012-03-14 20:40:09 -0400

    class7

--- class7/labs/dictionaries_again.py
+++ class7/labs/dictionaries_again.py
@@ -30,15 +30,16 @@ d["sally"] = "35"
 # print the value at the key named sally (#3)
 print d["sally"]
 # use the get method to retrieve the value for a non-existent key named alice, default it to 0 (#5); assign to a variable named alice_score
-
+alice_score = d.get("alice_score", 0)
 # print out alice_score
-
+print alice_score
 # print out all of the keys in the dictionary (#6)
-
+print d.keys()
 # print out all of the values in the dictionary (#7)
-
+print d.values()
 # print out all of the key/value pairs in the dictionary as a list of tuples (#8)
-
+print d.items()
 # create a dictionary called more_scores, this time with the following key/value pairs... "eve" - 23, "sam" - 39 (#2)
-
+more_scores = {"eve": "23", "sam": "39"}
 # print out more_scores
+print more_scores
