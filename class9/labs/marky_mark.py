a99f7c575a2c98400465387f7575e2c0709fc49c
Student <student@ENT-V321-007.local>
2012-03-28 18:54:26 -0400

    class9

--- class9/labs/marky_mark.py
+++ class9/labs/marky_mark.py
@@ -11,7 +11,7 @@ Code:
 1.  Bring in the markdown module 
 2.  Create a multi-line string with the following contents:
 * this is a list
-* of strings!
+*of strings!
 3.  This string represents a markdown list.  Convert the markdown list into html by calling the function specified in the documentation (http://packages.python.org/Markdown/reference.html#the-basics)
 
 Expected Output:
@@ -20,3 +20,7 @@ Expected Output:
 <li>of strings!</li>
 </ul>
 """
+import markdown
+m = """* this is a list
+*of strings!"""
+print markdown.markdown (m)
